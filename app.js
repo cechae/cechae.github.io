b87e4f7c716e0ec6e29afccc3e2ea166b6ce7456
spicysos <cchae216@gmail.com>
2020-01-20 23:50:41 -0500

    fix

--- app.js
+++ app.js
@@ -6,18 +6,8 @@ $( "#click-btn" ).click(function() {
   (function($) {
     "use strict"; // Start of use strict
   
-    // Floating label headings for the contact form
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-      $(this).removeClass("floating-label-form-group-with-focus");
-    });
-  
     // Show the navbar when the page is scrolled up
     var MQL = 992;
-  
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
       var headerHeight = $('#mainNav').height();
@@ -28,7 +18,7 @@ $( "#click-btn" ).click(function() {
           var currentTop = $(window).scrollTop();
           //check if user is scrolling up
           if (currentTop < this.previousTop) {
-            //if scrolling up...
+            //if scrolling up
             if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
               $('#mainNav').addClass('is-visible');
             } else {
